36be62535840b8c772862575c74529d2fb7bd3be
Reuben <rmoswela@gmail.com>
2023-12-11 23:31:04 +0000

    initial commit of all projects

---
+++ LiveNationAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace LiveNationAPI.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
