44e540a070579e155e417a63daf77d6a68f405c6
Reuben <rmoswela@gmail.com>
2023-12-12 02:17:46 +0000

    makes the failing test pass

--- LiveNationAPI/Controllers/LiveNationController.cs
+++ LiveNationAPI/Controllers/LiveNationController.cs
@@ -16,6 +16,11 @@ public class LiveNationController : ControllerBase
 
     public IActionResult Get(NumberRangeRequestDto numberRangeRequest)
     {
+        if (numberRangeRequest.From == 0 || numberRangeRequest.To == 0)
+        {
+            return BadRequest("The values have to be in a range of 1 and greater");
+        }
+        
         var response = _generator.GenerateResult(numberRangeRequest);
 
         var options = new JsonSerializerOptions {
