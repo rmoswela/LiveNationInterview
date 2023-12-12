88dffcad14c33d57e8eaaa494a4672e18a668db7
Reuben <rmoswela@gmail.com>
2023-12-12 01:29:57 +0000

    increases test scope

--- LiveNationAPI.Tests/RuleEvaluatorTests.cs
+++ LiveNationAPI.Tests/RuleEvaluatorTests.cs
@@ -16,23 +16,31 @@ public class Tests
     public void EvaluateRule_ForMultiplesOfThree_ShouldOutputLive()
     {
         Assert.That(_evaluator.EvaluateRule(3), Is.EqualTo("Live"));
+        Assert.That(_evaluator.EvaluateRule(6), Is.EqualTo("Live"));
+        Assert.That(_evaluator.EvaluateRule(18), Is.EqualTo("Live"));
     }
 
     [Test]
     public void EvaluateRule_ForMultiplesOfFive_ShouldOutputNation()
     {
         Assert.That(_evaluator.EvaluateRule(5), Is.EqualTo("Nation"));
+        Assert.That(_evaluator.EvaluateRule(10), Is.EqualTo("Nation"));
+        Assert.That(_evaluator.EvaluateRule(20), Is.EqualTo("Nation"));
     }
 
     [Test]
     public void EvaluateRule_ForMultiplesOfBothThreeAndFive_ShouldOutputLiveNation()
     {
         Assert.That(_evaluator.EvaluateRule(15), Is.EqualTo("LiveNation"));
+        Assert.That(_evaluator.EvaluateRule(30), Is.EqualTo("LiveNation"));
+        Assert.That(_evaluator.EvaluateRule(45), Is.EqualTo("LiveNation"));
     }
 
     [Test]
     public void EvaluateRule_OtherValues_ShouldOutputThoseOtherValues()
     {
         Assert.That(_evaluator.EvaluateRule(16), Is.EqualTo("16"));
+        Assert.That(_evaluator.EvaluateRule(53), Is.EqualTo("53"));
+        Assert.That(_evaluator.EvaluateRule(94), Is.EqualTo("94"));
     }
 }
